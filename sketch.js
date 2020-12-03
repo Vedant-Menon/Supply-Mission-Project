4842b16bb2e5072daa56cec772f478b65c7da1e1
Vedant-Menon <74135210+Vedant-Menon@users.noreply.github.com>
2020-12-03 18:08:02 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
-
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground;
 function preload()
 {
 	helicopterIMG=loadImage("helicopter.png")
@@ -32,7 +31,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:false});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -50,19 +49,23 @@ function draw() {
   rectMode(CENTER);
   background(0);
   packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y
-  
-  keyPressed();
+  packageSprite.y= packageBody.position.y 
+
+  keyPressed();;
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-     Body.setStatic(packageBody,false)
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+    Matter.Body.setStatic(packageBody,false)
     
   }
 }
+
+
+
 
 
 
